b657e39c0c686ceb66ac2377cb4fb8f112b31677
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-02-18 20:23:02 -0500

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ fs.readdir("./commands/", (err, files) => {
 });
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
-  bot.user.setActivity("!report", {type: "STREAMING"});
+  bot.user.setActivity("!report", {type: "WATCHING"});
 });
 
 bot.on("message", async message => {
