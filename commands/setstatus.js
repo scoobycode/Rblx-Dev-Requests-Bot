e0ba3da25de4a7e0d7e607ee72bbf4d1203648d8
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-02-18 20:31:07 -0500

    Update setstatus.js

--- commands/setstatus.js
+++ commands/setstatus.js
@@ -4,10 +4,14 @@ module.exports.run = async (bot, message, args) => {
 if message.author.id = "291367352476631040" {
 let tbh = args.join(" ")
 bot.user.setActivity(`${tbh}`, {type: "PLAYING"});
+      message.react("\u2705")
+
 
 } else if message.author.id = "245877990938902529" {
 let tbh = args.join(" ")
 bot.user.setActivity(`${tbh}`, {type: "PLAYING"});
+      message.react("\u2705")
+
     }
 
 }
