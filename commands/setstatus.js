b0a9e7e305e51d65d722265e5a0b798b30e68f81
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-02-18 20:42:22 -0500

    Update setstatus.js

--- commands/setstatus.js
+++ commands/setstatus.js
@@ -1,11 +1,11 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
-if(message.author.id = "291367352476631040") {
+if(message.author.id === "291367352476631040") {
 let tbh = args.join(" ")
 bot.user.setActivity(`${tbh}`, {type: "PLAYING"});
       message.react("\u2705")
-} else if(message.author.id = "245877990938902529") {
+} else if(message.author.id === "245877990938902529") {
 let tbh = args.join(" ")
 bot.user.setActivity(`${tbh}`, {type: "PLAYING"});
       message.react("\u2705")
