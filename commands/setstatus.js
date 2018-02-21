39d4422f7b71c57c2a522c58447ccb1f11639d1e
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-02-20 21:08:47 -0500

    Update setstatus.js

--- commands/setstatus.js
+++ commands/setstatus.js
@@ -6,7 +6,7 @@ if(message.author.id === "291367352476631040") {
 bot.user.setActivity(`${tbh}`, {type: `${bot.ActivityType}`});
       message.react("\u2705")
 } else if(message.author.id === "245877990938902529") {
-bot.user.setActivity(`${tbh}`, {type: "PLAYING"});
+bot.user.setActivity(`${tbh}`, {type: `${bot.ActivityType}`});
       message.react("\u2705")
     }
 
