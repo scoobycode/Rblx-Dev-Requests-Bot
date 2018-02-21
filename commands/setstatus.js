e752804f69180628937a01c6402572e03a90825a
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-02-21 06:25:38 -0500

    Update setstatus.js

--- commands/setstatus.js
+++ commands/setstatus.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 let tbh = args.join(" ")
 message.channel.send(`${bot.user.presence.game.type.toString()}`)
-if(message.author.id === "291367352476631040") {
+if((message.author.id === "291367352476631040") && (bot.user.presence.game.type.toString() === 0)) {
 bot.user.setActivity(`${tbh}`, {type: `${bot.user.presence.game.type.toString()}`});
       message.react("\u2705")
 } else if(message.author.id === "245877990938902529") {
