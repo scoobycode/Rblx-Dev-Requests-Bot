f823deb5abeaf650708e4ad5be1a06a629358f26
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-02-20 21:06:44 -0500

    Update setactivity.js

--- commands/setactivity.js
+++ commands/setactivity.js
@@ -3,10 +3,10 @@ module.exports.run = async (bot, message, args) => {
 let tbh = args.join(" ").toUpperCase()
 
 if(message.author.id === "291367352476631040") {
-bot.user.setActivity(`${bot.game.name}`, {type: `${tbh}`});
+bot.user.setActivity(`${bot.game}`, {type: `${tbh}`});
       message.react("\u2705")
 } else if(message.author.id === "245877990938902529") {
-bot.user.setActivity(`${bot.game.name}`, {type: `${tbh}`});
+bot.user.setActivity(`${bot.game}`, {type: `${tbh}`});
       message.react("\u2705")
     }
 
