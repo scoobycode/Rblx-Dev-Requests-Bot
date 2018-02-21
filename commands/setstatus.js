fcedc74e0f90e776a64275f1dbd9df413662ba49
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-02-21 11:39:21 -0500

    Update setstatus.js

--- commands/setstatus.js
+++ commands/setstatus.js
@@ -1,12 +1,14 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 let tbh = args.join(" ")
-message.channel.send(`${bot.user.presence.game.type.toString()}`)
 if((message.author.id === "291367352476631040") && (bot.user.presence.game.type === 0)) {
 bot.user.setActivity(`${tbh}`, {type: "PLAYING"});
       message.react("\u2705")
 } else if ((message.author.id === "291367352476631040") && (bot.user.presence.game.type === 1)) {
       bot.user.setActivity(`${tbh}`, {type: "STREAMING"});
+      message.react("\u2705")
+} else if ((message.author.id === "291367352476631040") && (bot.user.presence.game.type === -1)) {
+      bot.user.setActivity(`${tbh}`, {type: "PLAYING"});
       message.react("\u2705")
 } else if ((message.author.id === "291367352476631040") && (bot.user.presence.game.type === 2)) {
       bot.user.setActivity(`${tbh}`, {type: "LISTENING"});
@@ -16,6 +18,9 @@ bot.user.setActivity(`${tbh}`, {type: "PLAYING"});
       message.react("\u2705")
 } else if ((message.author.id === "245877990938902529") && (bot.user.presence.game.type === 0)) {
 bot.user.setActivity(`${tbh}`, {type: "PLAYING"});
+      message.react("\u2705")
+} else if ((message.author.id === "245877990938902529") && (bot.user.presence.game.type === -1)) {
+      bot.user.setActivity(`${tbh}`, {type: "PLAYING"});
       message.react("\u2705")
 } else if ((message.author.id === "245877990938902529") && (bot.user.presence.game.type === 1)) {
       bot.user.setActivity(`${tbh}`, {type: "STREAMING"});
