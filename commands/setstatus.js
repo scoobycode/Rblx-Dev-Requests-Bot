f4beeb2b82210678f29f682180cd53942b6bd5f0
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-02-20 20:11:45 -0500

    Update setstatus.js

--- commands/setstatus.js
+++ commands/setstatus.js
@@ -1,13 +1,56 @@
+async function awaitReply(message,question, limit = 300000){
+    const filter = m => m.author.id === message.author.id;
+    await message.author.send(question);
+    try {
+      const collected = await message.author.dmChannel.awaitMessages(filter, { max: 1, time: limit, errors: ['time'] });
+      return collected.first().content;
+    } catch (error) {
+return message.author.send("**Prompt Cancelled -- There Was No Response After Five Minutes**");
+    }
+  }
+
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
-let tbh = args.join(" ")
+message.react("\u2705")
+message.channel.send(`${message.author}, Prompt will continue in DMs! \uD83D\uDCEC`)
 
 if(message.author.id === "291367352476631040") {
-bot.user.setActivity(`${tbh}`, {type: "PLAYING"});
+  
+  const status = await awaitReply(message, "What should the status be?\nSay **cancel** to cancel prompt.", 300000);
+  if(status.toLowerCase() === "cancel") return message.author.send("**Prompt Cancelled**")
+  if(status === "**Prompt Cancelled -- There Was No Response After Five Minutes**") return bot.log("ok")
+  const activity = await awaitReply(message, "What should the activity be?\nChoices: **Playing**, **Listening**, **Watching**\nSay **cancel** to cancel prompt.", 300000);
+  if(activity.toLowerCase() == "cancel") return message.author.send("**Prompt Cancelled**")
+  if(activity === "**Prompt Cancelled -- There Was No Response After Five Minutes**") return bot.log("ok")
+  if(activity.toLowerCase() === "playing") {
+      bot.user.setActivity(`${status}`, {type: "PLAYING"});
       message.react("\u2705")
+  } else if (activity.toLowerCase() === "listening") {
+      bot.user.setActivity(`${status}`, {type: "LISTENING"});
+      message.react("\u2705")
+        } else if (activity.toLowerCase() === "watching") {
+      bot.user.setActivity(`${status}`, {type: "WATCHING"});
+      message.react("\u2705")
+        }     
+              
+              
 } else if(message.author.id === "245877990938902529") {
-bot.user.setActivity(`${tbh}`, {type: "PLAYING"});
+const status = await awaitReply(message, "What should the status be?\nSay **cancel** to cancel prompt.", 300000);
+  if(status.toLowerCase() === "cancel") return message.author.send("**Prompt Cancelled**")
+  if(status === "**Prompt Cancelled -- There Was No Response After Five Minutes**") return bot.log("ok")
+  const activity = await awaitReply(message, "What should the activity be?\nChoices: **Playing**, **Listening**, **Watching**\nSay **cancel** to cancel prompt.", 300000);
+  if(activity.toLowerCase() == "cancel") return message.author.send("**Prompt Cancelled**")
+  if(activity === "**Prompt Cancelled -- There Was No Response After Five Minutes**") return bot.log("ok")
+  if(activity.toLowerCase() === "playing") {
+      bot.user.setActivity(`${status}`, {type: "PLAYING"});
       message.react("\u2705")
+  } else if (activity.toLowerCase() === "listening") {
+      bot.user.setActivity(`${status}`, {type: "LISTENING"});
+      message.react("\u2705")
+        } else if (activity.toLowerCase() === "watching") {
+      bot.user.setActivity(`${status}`, {type: "WATCHING"});
+      message.react("\u2705")
+        }     
     }
 
 }
