575011673e7918add716d7994455e20d76a6bc6c
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-04 15:28:14 -0500

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-message.reply(`pong! \`${client.ping}ms\``)
+message.reply(`pong! \`${bot.ping}ms\``)
 
  }
 
