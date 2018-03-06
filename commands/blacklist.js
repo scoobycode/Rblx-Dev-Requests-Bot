5cdb2df404908e7edfa408317ca19fa3164ea80a
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-06 16:13:18 -0500

    Update blacklist.js

--- commands/blacklist.js
+++ commands/blacklist.js
@@ -1,16 +1,22 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
       let channel = bot.channels.find(`id`, "420677482287464448")
-      let marray = await channel.fetchMessages()
-      let aarray = marray.filter(m => RegExp(message.author.id, "gi").test(m.content));
-      let buser = aarray.first();
+      let pingeduser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+	let userid = args[0]
+      if(!pingeduser) {
+	      let aarray = await channel.fetchMessages()
+      	      let barray = aarray.filter(m => RegExp(userid, "gi").test(m.content));
+	      let auser = barray.first();
+	      if(!auser) return channel.send(userid)
+	      if(auser) return message.channel.send("This user is already blacklisted!")
+      } else
+	     let carray = await channel.fetchMessages()
+      	      let darray = carray.filter(m => RegExp(pingeduser.id, "gi").test(m.content));
+	      let buser = darray.first();
+	if(!buser) return channel.send(pingeduser.id)
 	if(buser) return message.channel.send("This user is already blacklisted!")
-      let auser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-		if(!auser) {
-			let cuser = args[0]
-			return channel.send(cuser)
-		}
-      channel.send(auser.id)
+}
+	
 
 
 }
