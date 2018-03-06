b525e2e94b326e505e73cc0208b3c73cc096ab18
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-06 15:48:24 -0500

    Update blacklist.js

--- commands/blacklist.js
+++ commands/blacklist.js
@@ -1,9 +1,9 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
        let channel = bot.channels.find(`id`, "420677482287464448")
-      marray = await channel.fetchMessages()
-	      aarray.filter(m => RegExp(message.author.id, "gi").test(m.startsWith()));
-			buser = aarray.first();
+      let marray = await channel.fetchMessages()
+	      let aarray.filter(m => RegExp(message.author.id, "gi").test(m.startsWith()));
+			let buser = aarray.first();
       
       if(buser) {
       return message.channel.send("blacklisted")
