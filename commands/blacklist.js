e9a155ef4578860e498ab391713c28fa7c75b902
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-06 15:36:55 -0500

    Create blacklist.js

---
+++ commands/blacklist.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+module.exports.run = async (bot, message, args) => {
+       let channel = client.channels.find(`id`, "420677482287464448")
+      marray = channel.fetchMessages().filter(m => RegExp(message.author.id, "gi").test(m.startsWith()));
+			buser = marray.first();
+      
+      if(buser) {
+      return message.channel.send("blacklisted")
+      } else return message.channel.send("not blacklisted")
+
+
+}
+
+module.exports.help = {
+	name: "blacklist"
+}
