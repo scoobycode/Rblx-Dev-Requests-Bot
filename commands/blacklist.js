78df0ad9930a506b5454670870a0a0b30832efa7
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-06 15:57:01 -0500

    Update blacklist.js

--- commands/blacklist.js
+++ commands/blacklist.js
@@ -7,6 +7,7 @@ module.exports.run = async (bot, message, args) => {
       let auser = message.guild.member(message.mentions.users.first().id || message.guild.members.get(args[0]).id);
 		if(!auser) {
 			auser = args[0]
+		}
       if(!buser) {
       channel.send(auser)
       } else return message.channel.send("This user is already blacklisted!")
