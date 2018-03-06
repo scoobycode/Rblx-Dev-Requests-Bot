8445a4bfc5e4a72dde9a2b19d92ba6b1afd96e91
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-06 18:20:05 -0500

    Create unblacklist.js

---
+++ commands/unblacklist.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js");
+module.exports.run = async (bot, message, args) => {
+      let channel = bot.channels.find(`id`, "420677482287464448")
+      let pingeduser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+	let userid = args[0]
+	let messages = await channel.fetchMessages()
+
+      if(!pingeduser) {
+      	      let barray = messages.filter(m => RegExp(userid, "gi").test(m.content));
+	      let auser = barray.first();
+	      if(auser) return channel.auser.delete()
+	      if(!auser) return message.channel.send("This user is not blacklisted!")
+      } else {
+      	      let darray = messages.filter(m => RegExp(pingeduser.id, "gi").test(m.content));
+	      let buser = darray.first();
+	if(buser) return channel.buser.delete()
+	if(!buser) return message.channel.send("This user is not blacklisted!")
+}
+	
+
+
+}
+
+module.exports.help = {
+	name: "unblacklist"
+}
