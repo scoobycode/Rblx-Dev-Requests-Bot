9fad7fe1042175af35d341ce97417f92065e5ae4
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-06 16:15:04 -0500

    Update blacklist.js

--- commands/blacklist.js
+++ commands/blacklist.js
@@ -3,15 +3,15 @@ module.exports.run = async (bot, message, args) => {
       let channel = bot.channels.find(`id`, "420677482287464448")
       let pingeduser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
 	let userid = args[0]
+	let messages = await channel.fetchMessages()
+
       if(!pingeduser) {
-	      let aarray = await channel.fetchMessages()
-      	      let barray = aarray.filter(m => RegExp(userid, "gi").test(m.content));
+      	      let barray = messages.filter(m => RegExp(userid, "gi").test(m.content));
 	      let auser = barray.first();
 	      if(!auser) return channel.send(userid)
 	      if(auser) return message.channel.send("This user is already blacklisted!")
       } else
-	     let carray = await channel.fetchMessages()
-      	      let darray = carray.filter(m => RegExp(pingeduser.id, "gi").test(m.content));
+      	      let darray = messages.filter(m => RegExp(pingeduser.id, "gi").test(m.content));
 	      let buser = darray.first();
 	if(!buser) return channel.send(pingeduser.id)
 	if(buser) return message.channel.send("This user is already blacklisted!")
