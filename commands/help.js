5d2c0ba69735a799fa1b1b26f7b0a70defdb1ec7
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-06 22:10:39 -0500

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,14 @@
+const Discord = require("discord.js");
+module.exports.run = async (bot, message, args) => {
+	let helper = new Discord.RichEmbed()
+	.setTitle("Commands")
+	.addField("report", "Starts a prompt to report a scammer and send that prompt to the RDR server")
+	.addField("invite", "Sends you my invite")
+	.addField("server", "Sends you the invite to the RDR server");
+
+	message.react("\u2705")
+	message.author.send(helper)
+}
+module.exports.help = {
+	name: "help"
+}
