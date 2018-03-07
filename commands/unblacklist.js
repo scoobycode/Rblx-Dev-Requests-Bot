186216fcb31d773053d9b5c99d2756b211f751f2
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-06 19:14:42 -0500

    Update unblacklist.js

--- commands/unblacklist.js
+++ commands/unblacklist.js
@@ -1,5 +1,7 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
+	if(message.author.id === "291367352476631040" || message.author.id === "245877990938902529" || message.author.id === "294990053849956354" || message.author.id === "303683211790254080" || message.author.id === "335096822194241537") {
+
       let channel = bot.channels.find(`id`, "420677482287464448")
       let pingeduser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
 	let userid = args[0]
@@ -23,7 +25,7 @@ module.exports.run = async (bot, message, args) => {
 	      if(!buser) return message.channel.send("This user is not blacklisted!")
 }
 	
-
+}
 
 }
 
