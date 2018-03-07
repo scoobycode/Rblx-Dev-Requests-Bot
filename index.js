89a89f3b893229fa39e1e29d9c370a68c41efb7d
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-06 21:56:59 -0500

    Update index.js

--- index.js
+++ index.js
@@ -40,12 +40,11 @@ if((message.content.endsWith("**MUST WAIT TO USE REPORT COMMAND**")) && (message
     .setColor("#0000ff")
     .description("Thanks for inviting Scam reports bot to your server!\nScam reports bot is owned by RDR and was made by the Co-Owner, @ethanlaj#8805. For a list of commands, just say \`!help\`\nIf you need any help what so ever, feel free to join our support server!\nInvite link: https://discord.gg/3dECRh8");
 
-
-  bot.on("guildCreate", async guild => {
+process.on('unhandledRejection', console.error)
+  bot.on("guildCreate", guild => {
     
   let hichannel = guild.channels.first()
    hichannel.send(hello)
-    .catch(err => console.log(err))
 });
   let prefix = botconfig.prefix;
   let messageArray = message.content.split(" ");
