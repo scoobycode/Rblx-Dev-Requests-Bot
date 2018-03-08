016273a6a292e3510667ad0eebe23d039ba8c2b8
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-07 21:41:30 -0500

    Update blacklist.js

--- commands/blacklist.js
+++ commands/blacklist.js
@@ -1,5 +1,7 @@
 const Discord = require("discord.js");
+const fs = require("fs");
 module.exports.run = async (bot, message, args) => {
+	let blacklist = require("../blacklist.json")
 	if(message.author.id === "291367352476631040" || message.author.id === "245877990938902529" || message.author.id === "294990053849956354" || message.author.id === "303683211790254080" || message.author.id === "335096822194241537") {
       let channel = bot.channels.find(`id`, "420677482287464448")
       let pingeduser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
@@ -7,26 +9,30 @@ module.exports.run = async (bot, message, args) => {
 	let messages = await channel.fetchMessages()
 
       if(!pingeduser) {
-      	      let barray = messages.filter(m => RegExp(userid, "gi").test(m.content));
-	      let auser = barray.first();
-	      if(!auser) {
+      	      if(!blacklist[userid]) {
 		      let userob = await bot.fetchUser(userid)
 			if(!userob) return message.reply("Couldn't find this user!")
-		 	channel.send(`${userid}, ${userob.username}#${userob.discriminator}`)
+			blacklist[userid] = {name: `${userid.username}#${userid.discriminator}`}
+		      fs.writeFile("../blacklist.json", JSON.stringify(blacklist), (err) => {
+						if (err) throw err;
+});
 		      message.react("\u2705")
 	      }
-	      if(auser) return message.reply("This user is already blacklisted!")
+	      if(blacklist[userid]) return message.reply("This user is already blacklisted!")
       } else {
       	      let darray = messages.filter(m => RegExp(pingeduser.id, "gi").test(m.content));
 	      let buser = darray.first();
-	if(!buser) {
+	if(!blacklist[pingeduser.id]) {
 		let userob = await bot.fetchUser(pingeduser.id)
 		if(!userob) message.reply("Couldn't find this user!")
-		 channel.send(`${pingeduser.id}, ${userob.username}#${userob.discriminator}`)
+		blacklist[userob.id] = {name: `${userob.username}#${userob.discriminator}`}
+				fs.writeFile("../blacklist.json", JSON.stringify(blacklist), (err) => {
+			if (err) throw err;
+});
 		 message.react("\u2705")
-	}
+	} else return message.reply("This user is already blacklisted!")
 }
-	
+
 }
 
 }
