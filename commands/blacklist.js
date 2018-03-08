ab9bc25517d6a0929a9c2eaf5a00ec2457121831
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-07 20:33:48 -0500

    Update blacklist.js

--- commands/blacklist.js
+++ commands/blacklist.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js");
+const fs = require("fs");
 module.exports.run = async (bot, message, args) => {
 	let blacklist = require("../blacklist.json")
 	if(message.author.id === "291367352476631040" || message.author.id === "245877990938902529" || message.author.id === "294990053849956354" || message.author.id === "303683211790254080" || message.author.id === "335096822194241537") {
@@ -11,7 +12,7 @@ module.exports.run = async (bot, message, args) => {
       	      if(!blacklist[userid]) {
 		      let userob = await bot.fetchUser(userid)
 			if(!userob) return message.reply("Couldn't find this user!")
-		 	channel.send(`${userid}, ${userob.username}#${userob.discriminator}`)
+		      fs.writeFile(blacklist, userid)
 		      message.react("\u2705")
 	      }
 	      if(blacklist[userid]) return message.reply("This user is already blacklisted!")
@@ -21,7 +22,7 @@ module.exports.run = async (bot, message, args) => {
 	if(!blacklist[pingeduser.id]) {
 		let userob = await bot.fetchUser(pingeduser.id)
 		if(!userob) message.reply("Couldn't find this user!")
-		 channel.send(`${pingeduser.id}, ${userob.username}#${userob.discriminator}`)
+		      fs.writeFile(blacklist, pingeduser.id)
 		 message.react("\u2705")
 	} else return message.reply("This user is already blacklisted!")
 }
