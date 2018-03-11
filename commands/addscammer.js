ad8e8e4aa35583229adb02ba10dffa3957d9ff39
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-11 17:36:19 -0400

    Update addscammer.js

--- commands/addscammer.js
+++ commands/addscammer.js
@@ -1,4 +1,6 @@
 const Discord = require("discord.js");
+const rbx = require('roblox-js');
+
 module.exports.run = async (bot, message, args) => {
 	if(message.author.id === "291367352476631040" || message.author.id === "245877990938902529" || message.author.id === "294990053849956354" || message.author.id === "303683211790254080" || message.author.id === "335096822194241537") {
       let channel = bot.channels.find(`id`, "420745256439513089")
@@ -8,8 +10,11 @@ module.exports.run = async (bot, message, args) => {
       if(userid) {
       	      let barray = messages.filter(m => RegExp(userid, "gi").test(m.content));
 	      let auser = barray.first();
+	      var user = await rbx.getIdFromUsername(userid).catch((err) => {
+   		 message.reply(`${err}. Could not find this user. If error persists, contact ethanlaj#8805.`);
+  });
 	      if(!auser) {
-		 	channel.send(`${userid}`)
+		 	channel.send(`${user}`)
 		      message.react("\u2705")
 	      }
 	      if(auser) return message.reply("This user is already in the scammer database!")
