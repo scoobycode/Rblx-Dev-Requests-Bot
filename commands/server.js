b7a39a3b346c5114d6be66ac276fb784c7f8792d
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-11 17:05:33 -0400

    Update server.js

--- commands/server.js
+++ commands/server.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
 
-message.reply("https://www.discord.gg/3dECRh8")
+message.reply("https://discord.gg/3dECRh8")
 
 
 
