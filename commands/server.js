cd4e54860630b14a3086d882d0de742da0dbd365
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-11 17:03:00 -0400

    Create server.js

---
+++ commands/server.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+
+message.reply("https://www.discord.gg/3dECRh8")
+
+
+
+
+
+}
+module.exports.help = {
+      name: "server"
+  }
