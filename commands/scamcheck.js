245b1de3f2741c753b144c2b18b41f8a4627e848
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-11 18:14:06 -0400

    Update scamcheck.js

--- commands/scamcheck.js
+++ commands/scamcheck.js
@@ -1,5 +1,8 @@
+const rbx = require('roblox-js');
+
 module.exports.run = async (bot, message, args) => {
     let channel = bot.channels.find(`id`, "420745256439513089")
+    
 let messages = await channel.fetchMessages()
 var errortf
 let user = await rbx.getIdFromUsername(args[0]).catch((err) => {
