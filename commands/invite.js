cc699e46bca7ef89c166cc279c1d49c0afe4c812
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-11 17:02:04 -0400

    Create invite.js

---
+++ commands/invite.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+
+message.reply("https://discordapp.com/oauth2/authorize?client_id=403769002700046340&permissions=347201&scope=bot")
+
+
+
+
+
+}
+module.exports.help = {
+      name: "invite"
+  }
