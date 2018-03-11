c4d36c5c02a1c248a9402a920591db1f433652c9
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-11 16:47:53 -0400

    Update blacklist.js

--- commands/blacklist.js
+++ commands/blacklist.js
@@ -1,35 +1,31 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 	if(message.author.id === "291367352476631040" || message.author.id === "245877990938902529" || message.author.id === "294990053849956354" || message.author.id === "303683211790254080" || message.author.id === "335096822194241537") {
+      let channel = bot.channels.find(`id`, "420677482287464448")
       let pingeduser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
 	let userid = args[0]
+	let messages = await channel.fetchMessages()
 
       if(!pingeduser) {
-	      let userob = await bot.fetchUser(userid)
-	      if(!userob) return message.reply("Couldn't find this user!")
-	      let note = userob.note
-	      	      if(!note) note = ("ha")
-
-	      if(note.includes("BLACKLISTED") === false) {
+      	      let barray = messages.filter(m => RegExp(userid, "gi").test(m.content));
+	      let auser = barray.first();
+	      if(!auser) {
+		      let userob = await bot.fetchUser(userid)
 			if(!userob) return message.reply("Couldn't find this user!")
-		      let add = "BLACKLISTED"
-		      let newnote = note.concat(add);
-		 	await userob.setNote(`newnote`)
+		 	channel.send(`${userid}, ${userob.username}#${userob.discriminator}`)
 		      message.react("\u2705")
-	      } else return message.reply("This user is already blacklisted!")
+	      }
+	      if(auser) return message.reply("This user is already blacklisted!")
       } else {
-      	      let userob = await bot.fetchUser(pingeduser.id)
-	      if(!userob) return message.reply("Couldn't find this user!")
-	      let note = userob.note
-	      	      if(!note) note = ("ha")
-
-	      if(note.includes("BLACKLISTED") === false) {
-		if(!userob) return message.reply("Couldn't find this user!")
-		      let add = "BLACKLISTED"
-		      let newnote = note.concat(add);
-		 	await userob.setNote(`newnote`)
-		      message.react("\u2705")
-	} else return message.reply("This user is already blacklisted!")
+      	      let darray = messages.filter(m => RegExp(pingeduser.id, "gi").test(m.content));
+	      let buser = darray.first();
+	if(!buser) {
+		let userob = await bot.fetchUser(pingeduser.id)
+		if(!userob) message.reply("Couldn't find this user!")
+		 channel.send(`${pingeduser.id}, ${userob.username}#${userob.discriminator}`)
+		 message.react("\u2705")
+	}
+	if(buser) return message.reply("This user is already blacklisted!")
 }
 	
 }
