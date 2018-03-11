8576207019707d98dd6e77657a498b2a7da135c5
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-11 18:06:40 -0400

    Update scamcheck.js

--- commands/scamcheck.js
+++ commands/scamcheck.js
@@ -1,8 +1,9 @@
 module.exports.run = async (bot, message, args) => {
     let channel = bot.channels.find(`id`, "420745256439513089")
 let messages = await channel.fetchMessages()
+var errortf
 let user = await rbx.getIdFromUsername(args[0]).catch((err) => {
-	    errortf = true
+	     errortf = true
    		return message.reply(`${err}. If error persists, contact support by doing !server.`);
 		      });
 	 if (errortf == true) return;
