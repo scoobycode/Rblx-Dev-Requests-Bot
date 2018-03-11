f91834cb70a09245f99ea2cb1faf5d91e5f50f51
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-11 18:08:08 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -4,7 +4,9 @@ module.exports.run = async (bot, message, args) => {
 	.setTitle("Commands")
 	.addField("report", "Starts a prompt to report a scammer and send that prompt to the RDR server")
 	.addField("invite", "Sends you my invite")
-	.addField("server", "Sends you the invite to the RDR server");
+	.addField("server", "Sends you the invite to the RDR server")
+	.addField("scamcheck", "Tells you whether the username you provided is in our scam database or not.");
+
 
 	message.react("\u2705")
 	message.author.send(helper)
