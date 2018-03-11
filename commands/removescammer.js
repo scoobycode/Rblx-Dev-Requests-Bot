17a9f665277b35e14eb2ed9c61537ff0c109991a
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-11 17:12:30 -0400

    Create removescammer.js

---
+++ commands/removescammer.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js");
+module.exports.run = async (bot, message, args) => {
+	if(message.author.id === "291367352476631040" || message.author.id === "245877990938902529" || message.author.id === "294990053849956354" || message.author.id === "303683211790254080" || message.author.id === "335096822194241537") {
+
+      let channel = bot.channels.find(`id`, "420745256439513089")
+	let userid = args[0]
+	let messages = await channel.fetchMessages()
+
+      if(userid) {
+      	      let barray = messages.filter(m => RegExp(userid, "gi").test(m.content));
+	      let auser = barray.first();
+	      if(auser) {
+		     auser.delete()
+		     message.react("\u2705")
+	      }
+	      if(!auser) return message.reply("This user is not in the scammers database!")
+      
+}
+	
+}
+
+}
+
+module.exports.help = {
+	name: "unblacklist"
+}
