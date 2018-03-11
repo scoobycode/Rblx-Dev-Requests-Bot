89c48120befa417eb46774562588a0e0a1195de0
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-11 17:54:34 -0400

    Update scamcheck.js

--- commands/scamcheck.js
+++ commands/scamcheck.js
@@ -1,9 +1,15 @@
 module.exports.run = async (bot, message, args) => {
     let channel = bot.channels.find(`id`, "420745256439513089")
 let messages = await channel.fetchMessages()
+let user = await rbx.getIdFromUsername(args[0]).catch((err) => {
+	    errortf = true
+   		 message.reply(`${err}. If error persists, contact ethanlaj#8805.`);
+		      });
+	 if (errortf == true) return;
 
-    let array = messages.filter(m => RegExp(args[0], "gi").test(m.content));
+    let array = messages.filter(m => RegExp(user, "gi").test(m.content));
 	      let auser = barray.first();
+	
 	     if(auser) return message.reply("This person is in our scammers database!")
 	if(!auser) return message.reply("This person is not in our scammers database!")
 
