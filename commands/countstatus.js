ba47f4e3a4a21a96afd596bc2c4b31772c37e094
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-13 18:22:14 -0400

    Create countstatus.js

---
+++ commands/countstatus.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+module.exports.run = async (bot, message, args) => {
+
+if(message.author.id === "291367352476631040") {
+bot.user.setActivity(`${bot.user.presence.game.name}`, {type: `${tbh}`});
+      message.react("\u2705")
+} else if(message.author.id === "245877990938902529") {
+bot.user.setActivity(`${bot.user.presence.game.name}`, {type: `${tbh}`});
+      message.react("\u2705")
+    }
+
+}
+
+module.exports.help = {
+    name: "countstatus"
+}
