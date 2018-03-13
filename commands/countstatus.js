c61ba51166805b8dc5efc2f1219b48dc59710d51
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-13 18:23:22 -0400

    Update countstatus.js

--- commands/countstatus.js
+++ commands/countstatus.js
@@ -2,10 +2,10 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
 if(message.author.id === "291367352476631040") {
-bot.user.setActivity(`${bot.user.presence.game.name}`, {type: `${tbh}`});
+bot.user.setActivity(`${bot.guilds.size} servers`, {type: "WATCHING"});
       message.react("\u2705")
 } else if(message.author.id === "245877990938902529") {
-bot.user.setActivity(`${bot.user.presence.game.name}`, {type: `${tbh}`});
+bot.user.setActivity(`${bot.guilds.size} servers`, {type: "WATCHING"});
       message.react("\u2705")
     }
 
