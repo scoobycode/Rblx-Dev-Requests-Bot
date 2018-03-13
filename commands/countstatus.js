1ab532f9fe8bcce3178f254d4d5cb9667604e202
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-13 18:36:22 -0400

    Update countstatus.js

--- commands/countstatus.js
+++ commands/countstatus.js
@@ -9,14 +9,16 @@ bot.user.setActivity(`${bot.guilds.size} servers`, {type: "WATCHING"});
 bot.user.setActivity(`${bot.guilds.size} servers`, {type: "WATCHING"});
       message.react("\u2705")
       bot.counter = true
-    } else if ((message.author.id === "291367352476631040") && (bot.counter === true))
-            bot.user.setActivity("for !help", {type: "WATCHING"});
-      bot.counter = false
-}     else if ((message.author.id === "245877990938902529") && (bot.counter === true))
+    } else if ((message.author.id === "291367352476631040") && (bot.counter === true)) {
             bot.user.setActivity("for !help", {type: "WATCHING"});
       bot.counter = false
 
+} else if ((message.author.id === "245877990938902529") && (bot.counter === true)) {
+            bot.user.setActivity("for !help", {type: "WATCHING"});
+      bot.counter = false
 }
+}
+
 
 module.exports.help = {
     name: "countstatus"
