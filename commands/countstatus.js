0b96e4efb009cb0b473d53d80fc354b0cabfabf1
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-13 18:33:03 -0400

    Update countstatus.js

--- commands/countstatus.js
+++ commands/countstatus.js
@@ -5,7 +5,7 @@ if((message.author.id === "291367352476631040") && (bot.counter === false)) {
 bot.user.setActivity(`${bot.guilds.size} servers`, {type: "WATCHING"});
       message.react("\u2705")
       bot.counter = true
-} else if(message.author.id === "245877990938902529") && (bot.counter === false)) {
+} else if((message.author.id === "245877990938902529") && (bot.counter === false)) {
 bot.user.setActivity(`${bot.guilds.size} servers`, {type: "WATCHING"});
       message.react("\u2705")
       bot.counter = true
