b5442be777898476ece1c318cc0bfe7a5ecd9b58
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-13 19:03:17 -0400

    Update index.js

--- index.js
+++ index.js
@@ -36,8 +36,8 @@ bot.on("guildCreate", guild => {
     .setColor("#0000ff")
     .setDescription("Thanks for inviting Scam reports bot to your server!\nScam reports bot is owned by RDR and was made by the Co-Owner, @ethanlaj#8805. For a list of commands, just say \`!help\`\nIf you need any help what so ever, feel free to join our support server!\nInvite link: https://discord.gg/3dECRh8");
 
-  let ahichannel = guild.channels.filter(`type`, "text")
-  let hichannel = ahichannel.first()
+  let hichannel = guild.channels.find(`type`, "text")
+  //let hichannel = ahichannel.first()
    if(hichannel) hichannel.send(hello)
     if(bot.counter) bot.user.setActivity(`${bot.guilds.size} servers`, {type: "WATCHING"});
 });
