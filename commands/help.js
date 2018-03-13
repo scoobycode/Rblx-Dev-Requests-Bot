9f3f5535693b8a6c6794f3ac9d9666bf8df5eeb7
scoobycode <35615829+scoobycode@users.noreply.github.com>
2018-03-12 21:15:07 -0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 	let helper = new Discord.RichEmbed()
 	.setTitle("Commands")
-	.addField("!report", "Starts a prompt to report a scammer and send that prompt to the RDR server")
+	.addField("!Report", "Starts a prompt to report a scammer and send that prompt to the RDR server")
 	.addField("!Invite", "Sends you my invite")
 	.addField("!Server", "Sends you the invite to the RDR server")
 	.addField("!ScamCheck", "Tells you whether the username you provided is in our scam database or not");
