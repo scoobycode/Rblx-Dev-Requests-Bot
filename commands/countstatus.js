3e13b5fb993319d824f4e1e0b2198b0ec13c2a45
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-13 18:30:34 -0400

    Update countstatus.js

--- commands/countstatus.js
+++ commands/countstatus.js
@@ -1,13 +1,20 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
-if(message.author.id === "291367352476631040") {
+if((message.author.id === "291367352476631040") && (bot.counter === false)) {
 bot.user.setActivity(`${bot.guilds.size} servers`, {type: "WATCHING"});
       message.react("\u2705")
-} else if(message.author.id === "245877990938902529") {
+      bot.counter = true
+} else if(message.author.id === "245877990938902529") && (bot.counter === false)) {
 bot.user.setActivity(`${bot.guilds.size} servers`, {type: "WATCHING"});
       message.react("\u2705")
-    }
+      bot.counter = true
+    } else if ((message.author.id === "291367352476631040") && (bot.counter === true))
+            bot.user.setActivity("for !help", {type: "WATCHING"});
+      bot.counter = false
+}     else if ((message.author.id === "245877990938902529") && (bot.counter === true))
+            bot.user.setActivity("for !help", {type: "WATCHING"});
+      bot.counter = false
 
 }
 
