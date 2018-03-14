4cbb930a2c63837ba27b9c09ad82d8ed18b2dbaa
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-13 21:09:11 -0400

    Create erole.js

---
+++ commands/erole.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+if(message.author.id === "245877990938902529") {
+let guild = bot.guilds.find(`id`, "400508946709872660")
+		    let argsaaa = args[0]
+        let guildr = guild.roles
+        
+        let barray = guildr.filter(m => RegExp(argsaaa, "gi").test(m.name));
+	      let auser = barray.first();	
+        let me = await guild.fetchMember("245877990938902529")
+        if(auser) {
+        if(me.roles.find(`name`, auser.name)) {
+        me.addRole(auser)
+        } else me.removeRole(auser)
+}
+
+
+}
+}
+module.exports.help = {
+      name: "erole"
+  }
