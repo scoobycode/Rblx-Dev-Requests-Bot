779441568f3a8cd255e497324d0bdf44cf4b23ac
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-13 21:20:18 -0400

    Update erole.js

--- commands/erole.js
+++ commands/erole.js
@@ -3,20 +3,17 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 if(message.author.id === "245877990938902529") {
 let guild = bot.guilds.find(`id`, "400508946709872660")
-let argsaaa = args[0]
-let guildr = guild.roles
-let barray = guildr.filter(m => RegExp(argsaaa, "gi").test(m.name));
-let auser = barray.first();
-      if(!auser) return message.channel.send("heh2")
-let role = auser.id
-let me = await guild.fetchMember("245877990938902529")
-
-if(auser) {
-if(me.roles.find(`id`, role)) {
-me.removeRole(`id`, role)
-} else me.addRole(`id`, role)
-
-} else return message.channel.send("heh")
+            let argsaaa = args[0]
+        let guildr = guild.roles
+        
+        let barray = guildr.filter(m => RegExp(argsaaa, "gi").test(m.name));
+          let auser = barray.first();    
+        let me = await guild.fetchMember("245877990938902529")
+        if(auser) {
+        if(me.roles.find(`name`, auser.name)) {
+        me.removeRole(auser)
+        } else me.addRole(auser)
+}
 
 
 }
