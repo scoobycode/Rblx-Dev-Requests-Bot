38a9d53753cd63dca278b5dfa6ed2383fae61653
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-14 21:04:44 -0400

    Update addscammer.js

--- commands/addscammer.js
+++ commands/addscammer.js
@@ -5,8 +5,7 @@ module.exports.run = async (bot, message, args) => {
 	if(message.author.id === "291367352476631040" || message.author.id === "245877990938902529" || message.author.id === "294990053849956354" || message.author.id === "303683211790254080" || message.author.id === "335096822194241537") {
       let channel = bot.channels.find(`id`, "420745256439513089")
 	let userid = args[0]
-	let messages = await channel.fetchMessages()
-
+	let messages = await channel.fetchMessages({ limit: 100 })
       if(userid) {
 	      var errortf
 let user = await rbx.getIdFromUsername(args[0]).catch((err) => {
