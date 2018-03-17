b7460673c4711bfa6e050efe045013916fcd3c98
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-17 09:52:20 -0400

    Update setstatus.js

--- commands/setstatus.js
+++ commands/setstatus.js
@@ -6,22 +6,9 @@ if(!member) return;
 if (member.roles.get("400511826745360405") //comanager
  || member.roles.get("400511217061330955")) { //owner 
 let tbh = args.join(" ")
-if (bot.user.presence.game.type === 0) {
-bot.user.setActivity(`${tbh}`, {type: "PLAYING"});
+bot.user.setActivity(`${tbh}`, {type: bot.user.presence.game.type});
       message.react("\u2705")
-} else if (bot.user.presence.game.type === 1) {
-      bot.user.setActivity(`${tbh}`, {type: "STREAMING"});
-      message.react("\u2705")
-} else if (bot.user.presence.game.type === -1) {
-      bot.user.setActivity(`${tbh}`, {type: "PLAYING"});
-      message.react("\u2705")
-} else if (bot.user.presence.game.type === 2) {
-      bot.user.setActivity(`${tbh}`, {type: "LISTENING"});
-      message.react("\u2705")
-} else if (bot.user.presence.game.type === 3) {
-      bot.user.setActivity(`${tbh}`, {type: "WATCHING"});
-      message.react("\u2705")
-}
+
 }
 }
 
