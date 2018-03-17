5f4d27136b15044cb511acfcf4a2a7abebb68f9c
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-16 21:58:08 -0400

    Update setstatus.js

--- commands/setstatus.js
+++ commands/setstatus.js
@@ -1,38 +1,28 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
+      let guild = bot.guilds.find(`id`, "400508946709872660")
+let member = await guild.fetchMember(message.author.id)
+if(!member) return;
+if (member.roles.get("400511826745360405") //comanager
+ || member.roles.get("400511217061330955")) { //owner 
 let tbh = args.join(" ")
-if((message.author.id === "291367352476631040") && (bot.user.presence.game.type === 0)) {
+if (bot.user.presence.game.type === 0) {
 bot.user.setActivity(`${tbh}`, {type: "PLAYING"});
       message.react("\u2705")
-} else if ((message.author.id === "291367352476631040") && (bot.user.presence.game.type === 1)) {
+} else if (bot.user.presence.game.type === 1) {
       bot.user.setActivity(`${tbh}`, {type: "STREAMING"});
       message.react("\u2705")
-} else if ((message.author.id === "291367352476631040") && (bot.user.presence.game.type === -1)) {
+} else if (bot.user.presence.game.type === -1) {
       bot.user.setActivity(`${tbh}`, {type: "PLAYING"});
       message.react("\u2705")
-} else if ((message.author.id === "291367352476631040") && (bot.user.presence.game.type === 2)) {
+} else if (bot.user.presence.game.type === 2) {
       bot.user.setActivity(`${tbh}`, {type: "LISTENING"});
       message.react("\u2705")
-} else if ((message.author.id === "291367352476631040") && (bot.user.presence.game.type === 3)) {
+} else if (bot.user.presence.game.type === 3) {
       bot.user.setActivity(`${tbh}`, {type: "WATCHING"});
       message.react("\u2705")
-} else if ((message.author.id === "245877990938902529") && (bot.user.presence.game.type === 0)) {
-bot.user.setActivity(`${tbh}`, {type: "PLAYING"});
-      message.react("\u2705")
-} else if ((message.author.id === "245877990938902529") && (bot.user.presence.game.type === -1)) {
-      bot.user.setActivity(`${tbh}`, {type: "PLAYING"});
-      message.react("\u2705")
-} else if ((message.author.id === "245877990938902529") && (bot.user.presence.game.type === 1)) {
-      bot.user.setActivity(`${tbh}`, {type: "STREAMING"});
-      message.react("\u2705")
-} else if ((message.author.id === "245877990938902529") && (bot.user.presence.game.type === 2)) {
-      bot.user.setActivity(`${tbh}`, {type: "LISTENING"});
-      message.react("\u2705")
-} else if ((message.author.id === "245877990938902529") && (bot.user.presence.game.type === 3)) {
-      bot.user.setActivity(`${tbh}`, {type: "WATCHING"});
-      message.react("\u2705")
-    }
-
+}
+}
 }
 
 module.exports.help = {
