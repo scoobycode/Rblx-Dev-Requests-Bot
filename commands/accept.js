72047f2fc878b3e080976c2b80610f485036e9e9
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-16 20:41:21 -0400

    Create accept.js

---
+++ commands/accept.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+let userid = args[0]
+if(!userid) return message.reply("Please provide a user ID!")
+let user = bot.fetchUser(userid)
+if(!user) return message.reply("Couldn't find user!")
+try {
+user.send("congrats")
+}
+catch (e) {
+message.reply("Couldn't DM this user!")
+}
+
+
+ }
+
+module.exports.help = {
+    name: "accept"
+}
