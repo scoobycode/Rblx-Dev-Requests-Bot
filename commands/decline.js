9b98fdc47f224e427d9ca73585b84bb3bc24f2bb
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-16 21:39:57 -0400

    Update decline.js

--- commands/decline.js
+++ commands/decline.js
@@ -1,18 +1,26 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
+let guild = bot.guilds.find(`id`, "400508946709872660")
+let member = await guild.fetchMember(message.author.id)
+if(!member) return;
+if (member.roles.get("400523390441619457") //mod
+ || member.roles.get("400512010590355458") //admin
+ || member.roles.get("415914501909774336") //head admin
+ || member.roles.get("400511826745360405") //comanager
+ || member.roles.get("400511217061330955")) { //owner 
 let userid = args[0]
 if(!userid) return message.reply("Please provide a user ID!")
 let user = await bot.fetchUser(userid)
 if(!user) return message.reply("Couldn't find user!")
 try {
-user.send(":x: **Scam Report Declined -- After reviewing your report, our moderators and admins have decided this is not a valid scam report. This user will not be added to our data base.** :x:")
- message.react("✅")
+await user.send(":x: **Scam Report Declined -- After reviewing your report, our moderators and admins have decided this is not a valid scam report. This user will not be added to our data base.** :x:")
+  message.react("✅")
 }
 catch (e) {
-message.reply("Couldn't DM this user!")
+return message.reply("Couldn't DM this user!")
 }
-
+}
 
  }
 
