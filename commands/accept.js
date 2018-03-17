e97f5f3a87e616cac1a36aa085c993a84544757d
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-16 20:43:32 -0400

    Update accept.js

--- commands/accept.js
+++ commands/accept.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 let userid = args[0]
 if(!userid) return message.reply("Please provide a user ID!")
-let user = bot.fetchUser(userid)
+let user = await bot.fetchUser(userid)
 if(!user) return message.reply("Couldn't find user!")
 try {
 user.send("congrats")
