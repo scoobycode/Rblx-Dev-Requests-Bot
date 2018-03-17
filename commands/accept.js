b01f3e43f6cffead0af2c0a4cfbbded7a76687d0
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-16 21:26:04 -0400

    Update accept.js

--- commands/accept.js
+++ commands/accept.js
@@ -4,11 +4,11 @@ module.exports.run = async (bot, message, args) => {
 let guild = bot.guilds.find(`id`, "400508946709872660")
 let member = guild.fetchMember(message.author.id)
 if(!member) return;
-if (member.roles.exists(`id`, "400523390441619457") //mod
- || member.roles.exists(`id`, "400512010590355458") //admin
- || member.roles.exists(`id`, "415914501909774336") //head admin
- || member.roles.exists(`id`, "400511826745360405") //comanager
- || member.roles.exists(`id`, "400511217061330955")) { //owner 
+if (member.roles.find(`id`, "400523390441619457") //mod
+ || member.roles.find(`id`, "400512010590355458") //admin
+ || member.roles.find(`id`, "415914501909774336") //head admin
+ || member.roles.find(`id`, "400511826745360405") //comanager
+ || member.roles.find(`id`, "400511217061330955")) { //owner 
 let userid = args[0]
 if(!userid) return message.reply("Please provide a user ID!")
 let user = await bot.fetchUser(userid)
