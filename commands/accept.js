393308cd12bb78d57d70d018054b5a5dace08405
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-16 20:49:30 -0400

    Update accept.js

--- commands/accept.js
+++ commands/accept.js
@@ -6,7 +6,8 @@ if(!userid) return message.reply("Please provide a user ID!")
 let user = await bot.fetchUser(userid)
 if(!user) return message.reply("Couldn't find user!")
 try {
-user.send("congrats")
+user.send(":white_check_mark: **Scam Report Accepted -- After reviewing your report, our moderators and admins have decided this is a valid scam report. This user will be added to our data base shortly.** :white_check_mark:")
+ message.react("✅")
 }
 catch (e) {
 message.reply("Couldn't DM this user!")
