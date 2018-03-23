c5f8b9b377a7d9e0c34d8224ea8c4764d20db9cf
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-22 20:49:47 -0400

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ bot.on("guildCreate", async guild => {
     .setDescription("Thanks for inviting Scam reports bot to your server!\nScam reports bot is owned by RDR and was made by the Co-Owner, @ethanlaj#8805. For a list of commands, just say \`!help\`\nIf you need any help what so ever, feel free to join our support server!\nInvite link: https://discord.gg/3dECRh8");
 
  let hichannels = guild.channels.filter(c => c.type === "text")
-  let fhichannel = hichannels.filter(c => c.permissionFor(bot.user).has("SEND_MESSAGES"));
+  let fhichannel = hichannels.filter(c => c.permissionsFor(bot.user).has("SEND_MESSAGES"));
   let hichannel = fhichannel.first()
   if(hichannel) {
     hichannel.send(hello)
