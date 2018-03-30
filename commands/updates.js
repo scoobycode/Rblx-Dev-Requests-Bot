e35bc65320e9cef67ec2f6676b98d2f5540395e9
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-29 22:20:33 -0400

    Update updates.js

--- commands/updates.js
+++ commands/updates.js
@@ -1,7 +1,7 @@
 module.exports.run = async (bot, message, args) => {
 
 let channel = bot.channels.find(`id`, "429099957232992256")
-let editor = channel.fetchMessage("429100390206668800")
+let editor = await channel.fetchMessage("429100390206668800")
 message.channel.send(editor.content)
 
 
