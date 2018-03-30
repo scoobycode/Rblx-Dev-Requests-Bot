e115f2400f888ad6d7abe3f75ae12fcef6c8584f
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-29 22:25:58 -0400

    Update setupdates.js

--- commands/setupdates.js
+++ commands/setupdates.js
@@ -1,10 +1,15 @@
 module.exports.run = async (bot, message, args) => {
+	let guild = bot.guilds.find(`id`, "400508946709872660")
+let member = await guild.fetchMember(message.author.id)
+if(!member) return;
 if (member.roles.get("400511826745360405") //comanager
  || member.roles.get("400511217061330955")) { //owner 
   let update = args.join(" ")
 let channel = bot.channels.find(`id`, "429099957232992256")
 let editor = await channel.fetchMessage("429100390206668800")
 await editor.edit(update)
+message.react("\u2705")
+
 
 }
 
