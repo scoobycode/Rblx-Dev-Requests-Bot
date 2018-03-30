aab476e4318b797f3b79e53ee94fdff7feb69873
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-30 19:37:41 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -10,9 +10,9 @@ if (member.roles.get("400523390441619457") //mod
  || member.roles.get("400511217061330955")) { //owner 
 	let helpera = new Discord.RichEmbed()
 	.setTitle("Commands")
-	.addField("Public", "\`!report\` - Starts a prompt to report a scammer and send that prompt to the RDR server\n\`!invite\` - Sends you my invite\n\`!server\` - Sends you the invite to the RDR server\n\`!updates\` - Gives you our latest updates to the bot\nGives you our bot's server count\n\`!scamcheck\` - Tells you whether the username you provided is in our scam database or not\n\`!prefix\` - Changes my prefix in your server")
-	.addField("Moderator+", "\`!accept\` - Accepts a user's scam report\n\`!decline\` - Declines a user's scam report\n\`!addscammer\` - Adds a scammer to the scammers database\n\`!removescammer\` - Removes a scammer from the scammers database\n\`!blacklist\` - Blacklists a user from using the report command\n\`!unblacklist\` - Unblacklists a user from using the report command\n\`!guildblacklist\` - Blacklists a server from using the report command\n\`!guildunblacklist\` - Unblacklists a server from using the report command\n\`!open\` - Opens up the report command\n\`!close\` - Closes the report command")
-	.addField("Co-Owner+", "\`!setstatus\` - Sets the bot's status\n\`!setactivity\` - Sets the bot's activity\n\`!countstatus\` - Sets the bot's status to the server count\n\`!setupdates\` - Sets up the update command response")
+	.addField("Public", "\`${prefix}report\` - Starts a prompt to report a scammer and send that prompt to the RDR server\n\`${prefix}invite\` - Sends you my invite\n\`${prefix}server\` - Sends you the invite to the RDR server\n\`${prefix}updates\` - Gives you our latest updates to the bot\nGives you our bot's server count\n\`${prefix}scamcheck\` - Tells you whether the username you provided is in our scam database or not\n\`${prefix}prefix\` - Changes my prefix in your server")
+	.addField("Moderator+", "\`${prefix}accept\` - Accepts a user's scam report\n\`${prefix}decline\` - Declines a user's scam report\n\`${prefix}addscammer\` - Adds a scammer to the scammers database\n\`${prefix}removescammer\` - Removes a scammer from the scammers database\n\`${prefix}blacklist\` - Blacklists a user from using the report command\n\`${prefix}unblacklist\` - Unblacklists a user from using the report command\n\`${prefix}guildblacklist\` - Blacklists a server from using the report command\n\`${prefix}guildunblacklist\` - Unblacklists a server from using the report command\n\`${prefix}open\` - Opens up the report command\n\`${prefix}close\` - Closes the report command")
+	.addField("Co-Owner+", "\`${prefix}setstatus\` - Sets the bot's status\n\`${prefix}setactivity\` - Sets the bot's activity\n\`${prefix}countstatus\` - Sets the bot's status to the server count\n\`${prefix}setupdates\` - Sets up the update command response")
 	try
 	{
 	await message.author.send(helpera)
@@ -26,13 +26,13 @@ if (member.roles.get("400523390441619457") //mod
 } else {
 	let helper = new Discord.RichEmbed()
 	.setTitle("Commands")
-	.addField("!report", "Starts a prompt to report a scammer and send that prompt to the RDR server")
-	.addField("!invite", "Sends you my invite")
-	.addField("!server", "Sends you the invite to the RDR server")
-	.addField("!updates", "Gives you our latest updates to the bot")
-	.addField("!count", "Gives you our bot's server count")
-	.addField("!prefix", "Changes my prefix in your server")
-	.addField("!scamcheck", "Tells you whether the username you provided is in our scam database or not");
+	.addField("${prefix}report", "Starts a prompt to report a scammer and send that prompt to the RDR server")
+	.addField("${prefix}invite", "Sends you my invite")
+	.addField("${prefix}server", "Sends you the invite to the RDR server")
+	.addField("${prefix}updates", "Gives you our latest updates to the bot")
+	.addField("${prefix}count", "Gives you our bot's server count")
+	.addField("${prefix}prefix", "Changes my prefix in your server")
+	.addField("${prefix}scamcheck", "Tells you whether the username you provided is in our scam database or not");
 	
 	try {
 	await message.author.send(helper)
