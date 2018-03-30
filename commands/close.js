a30fb022d4898eb9e47628108c067d5dfd656e23
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-29 21:53:41 -0400

    Create close.js

---
+++ commands/close.js
@@ -0,0 +1,19 @@
+module.exports.run = async (bot, message, args) => {
+let guild = bot.guilds.find(`id`, "400508946709872660")
+let member = await guild.fetchMember(message.author.id)
+if(!member) return;
+if (member.roles.get("400523390441619457") //mod
+ || member.roles.get("400512010590355458") //admin
+ || member.roles.get("415914501909774336") //head admin
+ || member.roles.get("400511826745360405") //comanager
+ || member.roles.get("400511217061330955")) { //owner 
+let pchannel = bot.channels.find(`id`, "411246419979141121")
+await pchannel.setTopic("closed")
+
+
+}
+}
+
+module.exports.help = {
+    name: "close"
+}
