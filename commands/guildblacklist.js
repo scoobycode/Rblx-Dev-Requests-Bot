2622a1444fc087afe8453c79cc9bcf96553ea015
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-29 23:08:57 -0400

    Create guildblacklist.js

---
+++ commands/guildblacklist.js
@@ -0,0 +1,42 @@
+const Discord = require("discord.js");
+module.exports.run = async (bot, message, args) => {
+let guild = bot.guilds.find(`id`, "400508946709872660")
+let member = await guild.fetchMember(message.author.id)
+if(!member) return;
+if (member.roles.get("400523390441619457") //mod
+ || member.roles.get("400512010590355458") //admin
+ || member.roles.get("415914501909774336") //head admin
+ || member.roles.get("400511826745360405") //comanager
+ || member.roles.get("400511217061330955")) { //owner 
+	let channel = bot.channels.find(`id`, "420677482287464448")
+	let userid = args[0]
+  let guild = bot.guilds.find(`id`, userid)
+  if(!guild) return message.reply("Guild not found!")
+	let messages = await channel.fetchMessages({ limit: 100 })
+
+      if(userid) {
+      	      let barray = messages.filter(m => RegExp(userid, "gi").test(m.content));
+	      let auser = barray.first();
+	      if(!auser) {
+		 	channel.send(`${userid}`)
+		      message.react("\u2705")
+		      let mod = bot.channels.find(`id`, "418531258344275978")
+ let thing = new Discord.RichEmbed()
+	.setTitle("Guild Blacklisted")
+ .setColor("#FF0000")
+ .addField("Time Blacklisted", message.createdAt)
+	.addField("Moderator", message.author)
+	.addField("Guild Blacklisted", guild.name)
+  .addField("Guild ID", guild.id)
+ await mod.send(thing)
+	      }	else return message.reply("This guild is already blacklisted!")
+    
+}
+}	
+}
+
+
+
+module.exports.help = {
+	name: "blacklist"
+}
