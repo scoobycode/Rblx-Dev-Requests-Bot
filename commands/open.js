f48614573bf98214bf2fc837ef0c853e1fce980a
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-30 16:45:26 -0400

    Update open.js

--- commands/open.js
+++ commands/open.js
@@ -1,3 +1,5 @@
+const Discord = require("discord.js");
+
 module.exports.run = async (bot, message, args) => {
 let guild = bot.guilds.find(`id`, "400508946709872660")
 let member = await guild.fetchMember(message.author.id)
