6ae169f9c386813f61c8a8661f399a09d7a083c2
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-29 22:19:04 -0400

    Create setupdates.js

---
+++ commands/setupdates.js
@@ -0,0 +1,15 @@
+module.exports.run = async (bot, message, args) => {
+if (member.roles.get("400511826745360405") //comanager
+ || member.roles.get("400511217061330955")) { //owner 
+  let update = args.join(" ")
+let channel = bot.channels.find(`id`, "429099957232992256")
+let editor = channel.fetchMessage("429100390206668800")
+await editor.edit(update)
+
+}
+
+
+
+module.exports.help = {
+	name: "updates"
+}
