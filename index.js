988aa2d8a9dba3be3ac1cf698a06526d1f3d0e60
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-03-30 21:13:31 -0400

    Update index.js

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ bot.on("message", async message => {
   }
   if((message.isMemberMentioned(bot.user)) && (message.content.endsWith("prefix reset")) && (message.member.hasPermission("MANAGE_GUILD"))) {
 
-	let aaa = dbguild.channels.filter(m => RegExp("wbotprefixes-database", "gi").test(m.name));
+	let aaa = dbguild.channels.filter(m => RegExp("prefix-database", "gi").test(m.name));
 aaa.forEach(chl => {
   chl.fetchMessages({ limit: 100 }).then(msgs => {
       msgs.forEach(msg => {
