41794201eaac9934e106a921df94d53849f6253f
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-04-03 18:59:11 -0400

    Update report.js

--- commands/report.js
+++ commands/report.js
@@ -84,12 +84,13 @@ let darray = ttmessages.filter(m => RegExp(message.author.id, "gi").test(m.conte
 		if(m.content.toLowerCase() === "**Prompt Cancelled -- There Was No Response After Five Minutes**") {
 			duser.delete()
 		}
+		});
 	collector.on('end', collected => {
 		let aproof = collected.filter(m => m.attachments.first())
 		let bproof = aproof.array()
 		resolve(bproof.map(attatchment => attatchment.myURL).join(", "))
 		})
-	});
+	
 	});
 	
 	
