cc776d8592233d9d9ca0a03c2fbc308106f32ad0
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-04-05 21:20:57 -0400

    Update accept.js

--- commands/accept.js
+++ commands/accept.js
@@ -19,7 +19,7 @@ let userid = delmessage.embeds[0].fields[5].value
 let user = await bot.fetchUser(userid)
 if(!user) return message.reply("Couldn't find user!")
 try {
-await user.send(":white_check_mark: **Scam Report Accepted -- After reviewing your report, our moderators and admins have decided this is a valid scam report. This user will be added to our data base shortly.** :white_check_mark:")
+await user.send(":white_check_mark: **Scam Report Accepted -- After reviewing your report, our moderators and admins have decided this is a valid scam report. This user will be added to our database shortly.** :white_check_mark:")
   message.react("✅")
  let mod = bot.channels.find(`id`, "418531258344275978")
  let thing = new Discord.RichEmbed()
