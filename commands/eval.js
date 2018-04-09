9730e3cf4bdc53f5e29418afb629cc8ecef163c6
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-04-08 20:32:17 -0400

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 function clean(text) {
   if (typeof(text) === "string")
     return text.replace(//g, "" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
-  else
+  else {
       return text;
 }
 
