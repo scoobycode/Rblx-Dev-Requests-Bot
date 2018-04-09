170cd0f146e89839800cc5eb788ad68ee0193830
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-04-08 20:35:17 -0400

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -2,8 +2,8 @@ const Discord = require("discord.js");
 //thanks gtc
 function clean(text) {
   if (typeof(text) === "string")
-    return text.replace(//g, "" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
-  else {
+    return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+  else
       return text;
 }
 
@@ -11,7 +11,7 @@ let rejectionembed = new Discord.RichEmbed()
   .setDescription("nah bro")
 
 module.exports.run = async (bot, message, args) => {
-  if((message.author.id != "245877990938902529") && (message.author.id != "291367352476631040")) return message.channel.send(rejectionembed);
+  if(message.author.id != "245877990938902529" && message.author.id != "291367352476631040") return message.channel.send(rejectionembed);
   try {
     const code = args.join(" ");
     let evaled = eval(code);
@@ -19,7 +19,7 @@ module.exports.run = async (bot, message, args) => {
       evaled = require("util").inspect(evaled);
       message.channel.send(clean(evaled), {code:"xl"});
   } catch (err) {
-    message.channel.send(\ERROR` ```xl\n${clean(err)}\n````);
+    message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
   }
 }
 module.exports.help = {
