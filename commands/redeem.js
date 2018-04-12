c6d3dd719500994eef88f5582dd3c6e5ee3b1e31
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-04-12 17:44:10 -0400

    Create redeem.js

---
+++ commands/redeem.js
@@ -0,0 +1,27 @@
+const Discord = require("discord.js");
+var random = require('random-string');
+module.exports.run = async (bot, message, args) => {
+let userid = message.author.id
+let userschannel = bot.channels.find(`id`, "433791777468841996")
+let pusers = await userschannel.fetchMessages({
+        limit: 100
+})
+let usercheck = pusers.find(m => m.content === userid)
+if(usercheck) return message.reply("You already have premium!")
+let code = args[0]
+if(!code) return message.reply("Please include the code to redeem!")
+let codeschannel = bot.channels.find(`id`, "433791740387000341")
+                let codes = await codeschannel.fetchMessages({
+                        limit: 100
+                })
+                if(!codes) return message.reply("Not a valid code!")
+                let matchedcode = codes.find(m => m.content === code)
+                if(!matchedcode) return message.reply("Not a valid code!")
+                await matchedcode.delete()
+                await userschannel.send(message.author.id)
+                message.reply("You now have premium!")
+
+}
+module.exports.help = {
+  	name: "redeem"
+  }
