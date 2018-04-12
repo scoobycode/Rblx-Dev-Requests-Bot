44dca88fd75629ec10798097e81e4065014bf0d1
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-04-12 17:43:48 -0400

    Create puser.js

---
+++ commands/puser.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+module.exports.run = async (bot, message, args) => {
+  if(message.author.id !== "245877990938902529" && message.author.id !== "291367352476631040") return;
+
+let mentioneduser = message.mentions.users.first()
+if(!mentioneduser) return message.reply("Please mention a user!")
+let userschannel = bot.channels.find(`id`, "433791777468841996")
+let pusers = await userschannel.fetchMessages({
+        limit: 100
+})
+let usercheck = pusers.find(m => m.content === mentioneduser.id)
+if(usercheck) {
+  await usercheck.delete()
+  return message.reply("Removed this user's premium.")
+}
+                await userschannel.send(mentioneduser.id)
+                await message.reply("Successfully given this user premium!")
+
+}
+module.exports.help = {
+  	name: "puser"
+  }
