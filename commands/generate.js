d3a59dbee4cb3cf1df28ce8f0bc331c0f7d18c9e
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-04-12 18:03:15 -0400

    Update generate.js

--- commands/generate.js
+++ commands/generate.js
@@ -8,7 +8,7 @@ if(!number) return message.reply("Please include the number of codes to generate
 if(number > 50) return message.reply("Number must be smaller than 51!")
 let codeschannel = bot.channels.find(`id`, "433791740387000341")
 let i = 0
-while (i < number) {
+while (i < num) {
     var a = random(10)
     await codeschannel.send(a)
     await message.author.send(`${a},`)
