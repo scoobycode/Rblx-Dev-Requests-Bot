c249fad16b9cd96cdfe51e9f7d8a259875ee15ed
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-04-12 18:01:09 -0400

    Update generate.js

--- commands/generate.js
+++ commands/generate.js
@@ -8,7 +8,7 @@ if(!number) return message.reply("Please include the number of codes to generate
 if(number > 10) return message.reply("Too big of a number!")
 let codeschannel = bot.channels.find(`id`, "433791740387000341")
 let i = 0
-while (i < num) {
+while (i < number) {
     var a = random(10)
     await codeschannel.send(a)
     await message.author.send(`${a},`)
