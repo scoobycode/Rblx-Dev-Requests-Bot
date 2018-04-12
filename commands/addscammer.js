15ed4ddf1259aa6bf041a45d6a08eb3f4b5c4978
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-04-12 17:56:05 -0400

    Update addscammer.js

--- commands/addscammer.js
+++ commands/addscammer.js
@@ -22,8 +22,7 @@ let user = await rbx.getIdFromUsername(args[0]).catch((err) => {
    		return message.reply(`${err}. If error persists, contact support by doing !server.`);
 		      });
 	 if (errortf == true) return;
-      	      let barray = messages.filter(m => RegExp(user, "gi").test(m.content));
-	      let auser = barray.first();
+      	      let auser = messages.find(m => m.content === user);
 	      
 	      if(!auser) {
 		 	channel.send(`${user}`)
