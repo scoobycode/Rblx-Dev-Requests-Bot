3b560a59657817f74e7032c4a13661678f359533
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-04-12 17:43:31 -0400

    Create generate.js

---
+++ commands/generate.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+var random = require('random-string');
+module.exports.run = async (bot, message, args) => {
+  if(message.author.id !== "245877990938902529" && message.author.id !== "291367352476631040") return;
+let num = args[0]
+let number = Number(num)
+if(!number) return message.reply("Please include the number of codes to generate!")
+if(number > 10) return message.reply("Too big of a number!")
+let codeschannel = bot.channels.find(`id`, "433791740387000341")
+let i = 0
+while (i < num) {
+    var a = random(10)
+    await codeschannel.send(a)
+    await message.author.send(`${a},`)
+    i++;
+}
+message.reply("All Done!")
+
+
+
+}
+module.exports.help = {
+  	name: "generate"
+  }
