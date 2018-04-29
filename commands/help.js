04188bb6eefcf602209d9203000aecb19f73a881
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-04-29 17:39:55 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,15 +1,8 @@
 async function sendIfNot (bot, message, args, prefix) {
 let helper = new Discord.RichEmbed()
 	.setTitle("Commands")
-	.addField(`${prefix}report`, `Starts a prompt to report a scammer and send that prompt to the RDR server`)
-	.addField(`${prefix}invite`, `Sends you my invite`)
-	.addField(`${prefix}server`, `Sends you the invite to the RDR server`)
-	.addField(`${prefix}updates`, `Gives you our latest updates to the bot`)
-	.addField(`${prefix}count`, `Gives you our bot\`s server count`)
-	.addField(`${prefix}setprefix`, `Changes my prefix in your server`)
-	.addField(`${prefix}cancelreport`, `Removes your unread scam report`)
-	.addField(`${prefix}scamcheck`, `Tells you whether the username you provided is in our scam database or not`);
-	
+	.addField("Public", `\`${prefix}report\` - Starts a prompt to report a scammer and send that prompt to the RDR server\n\`${prefix}invite\` - Sends you my invite\n\`${prefix}server\` - Sends you the invite to the RDR server\n\`${prefix}updates\` - Gives you our latest updates to the bot\nGives you our bot's server count\n\`${prefix}scamcheck\` - Tells you whether the username you provided is in our scam database or not\n\`${prefix}setprefix\` - Changes my prefix in your server\n\`${prefix}cancelreport\` - Removes your unread scam report\n\`${prefix}verify\` - Verifys your roblox account to your discord account\n\`${prefix}getinfo\` - Gets a users roblox information`)
+	.addField("Premium", `\`${prefix}request\` - Requests something to be developed for you\n\`${prefix}banscammers\` - Bans all scammers from your server`);
 	try {
 	await message.author.send(helper)
 	}
@@ -41,7 +34,8 @@ if (member && member.roles.get("400523390441619457") //mod
  || member.roles.get("400511217061330955")) { //owner 
 	let helpera = new Discord.RichEmbed()
 	.setTitle("Commands")
-	.addField("Public", `\`${prefix}report\` - Starts a prompt to report a scammer and send that prompt to the RDR server\n\`${prefix}invite\` - Sends you my invite\n\`${prefix}server\` - Sends you the invite to the RDR server\n\`${prefix}updates\` - Gives you our latest updates to the bot\nGives you our bot's server count\n\`${prefix}scamcheck\` - Tells you whether the username you provided is in our scam database or not\n\`${prefix}setprefix\` - Changes my prefix in your server\n\`${prefix}cancelreport\` - Removes your unread scam report`)
+	.addField("Public", `\`${prefix}report\` - Starts a prompt to report a scammer and send that prompt to the RDR server\n\`${prefix}invite\` - Sends you my invite\n\`${prefix}server\` - Sends you the invite to the RDR server\n\`${prefix}updates\` - Gives you our latest updates to the bot\nGives you our bot's server count\n\`${prefix}scamcheck\` - Tells you whether the username you provided is in our scam database or not\n\`${prefix}setprefix\` - Changes my prefix in your server\n\`${prefix}cancelreport\` - Removes your unread scam report\n\`${prefix}verify\` - Verifys your roblox account to your discord account\n\`${prefix}getinfo\` - Gets a users roblox information`)
+	.addField("Premium", `\`${prefix}request\` - Requests something to be developed for you\n\`${prefix}banscammers\` - Bans all scammers from your server`)
 	.addField("Moderator+", `\`${prefix}accept\` - Accepts a user's scam report\n\`${prefix}decline\` - Declines a user's scam report\n\`${prefix}addscammer\` - Adds a scammer to the scammers database\n\`${prefix}removescammer\` - Removes a scammer from the scammers database\n\`${prefix}blacklist\` - Blacklists a user from using the report command\n\`${prefix}unblacklist\` - Unblacklists a user from using the report command\n\`${prefix}guildblacklist\` - Blacklists a server from using the report command\n\`${prefix}guildunblacklist\` - Unblacklists a server from using the report command\n\`${prefix}open\` - Opens up the report command\n\`${prefix}close\` - Closes the report command`)
 	.addField("Co-Owner+", `\`${prefix}setstatus\` - Sets the bot's status\n\`${prefix}setactivity\` - Sets the bot's activity\n\`${prefix}countstatus\` - Sets the bot's status to the server count\n\`${prefix}setupdates\` - Sets up the update command response`)
 	try
