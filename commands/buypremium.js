c008c58782cc8f52c326b43bd320709585735c9a
scoobycode <35615829+scoobycode@users.noreply.github.com>
2018-05-07 17:20:37 -0700

    Create buypremium.js

---
+++ commands/buypremium.js
@@ -0,0 +1,16 @@
+
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+
+message.reply("**You can buy premium here!:** https://selly.gg/p/3442c00e")
+
+
+
+
+
+}
+module.exports.help = {
+      name: "invite"
+  }
