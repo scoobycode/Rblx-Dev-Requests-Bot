c9e658829037800a92f637ce215c56ecb48d16f9
scoobycode <35615829+scoobycode@users.noreply.github.com>
2018-05-09 18:25:43 -0700

    Update server.js

--- commands/server.js
+++ commands/server.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
 
-message.reply("**Roblox Dev Requests Server:** https://discord.gg/3dECRh8")
+message.reply("**Roblox Dev Requests Server:** https://discord.gg/53Jgcu5")
 
 
 
