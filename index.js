19d4fce9371b45e536fbbe3968afb087be386d51
scoobycode <35615829+scoobycode@users.noreply.github.com>
2018-05-09 18:22:26 -0700

    Update index.js

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ bot.on("guildCreate", async guild => {
 	let hello = new Discord.RichEmbed()
 		.setTitle("Thanks For Adding Me To Your Server!")
 		.setColor("#0000ff")
-		.setDescription("Thanks for inviting Scam reports bot to your server!\nScam reports bot is owned by RDR and was made by the Co-Owner, @ethanlaj#8805. For a list of commands, just say \`!help\`\nIf you need any help what so ever, feel free to join our support server!\nInvite link: https://discord.gg/3dECRh8");
+		.setDescription("Thanks for inviting Scam reports bot to your server!\nScam reports bot is owned by Scooby and ethan and was made by the Co-Owner, @ethanlaj#8805. For a list of commands, just say \`!help\`\nIf you need any help what so ever, feel free to join our support server!\nInvite link: https://discord.gg/53Jgcu5");
 	let hichannels = guild.channels.filter(c => c.type === "text")
 	let ahichannels = hichannels.filter(c => c.permissionsFor(bot.user).has("READ_MESSAGES"));
 	let fhichannel = ahichannels.filter(c => c.permissionsFor(bot.user).has("SEND_MESSAGES"));
