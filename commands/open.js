a27ea02a956abfe3ac83245d585961a17f323750
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-05-11 19:34:38 -0400

    Update open.js

--- commands/open.js
+++ commands/open.js
@@ -1,14 +1,12 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-let guild = bot.guilds.find(`id`, "400508946709872660")
-let member = await guild.fetchMember(message.author.id)
-if(!member) return;
-if (member.roles.get("400523390441619457") //mod
- || member.roles.get("400512010590355458") //admin
- || member.roles.get("415914501909774336") //head admin
- || member.roles.get("400511826745360405") //comanager
- || member.roles.get("400511217061330955")) { //owner 
+let guild = bot.guilds.find(`id`, "443867131721941005");;
+	let member = await guild.fetchMember(message.author.id)
+	if (!member) return;
+	if (member && member.roles.get("443898332029517824") //helper
+ 	|| member.roles.get("443903247502147596") //moderator
+ 	|| member.roles.get("443867603103121410")) { //developer
 let pchannel = bot.channels.find(`id`, "444633860769185832")
 await pchannel.setTopic("open")
  message.react(`✅`)
