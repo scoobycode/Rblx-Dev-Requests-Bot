a9b29f7faa3b777e22c0884adee3a66dadfb9145
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-05-11 16:21:13 -0400

    Update resetcase.js

--- commands/resetcase.js
+++ commands/resetcase.js
@@ -2,7 +2,7 @@ module.exports.run = async (bot, message, args) => {
 
 if(message.author.id !== "245877990938902529") return;
 let casechannel = bot.channels.find(`id`, "444588562793627668")
-		let casenu = await casechannel.fetchMessage("431610688364871681")
+		let casenu = await casechannel.fetchMessage("444593973764292618")
 		await casenu.edit("1")
                     message.react("✅")
     }
