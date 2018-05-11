f51d1d990c8289169e677f07ad73e8d8a8015cd0
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-05-11 15:59:38 -0400

    Update banscammers.js

--- commands/banscammers.js
+++ commands/banscammers.js
@@ -7,9 +7,9 @@ module.exports.run = async (bot, message, args) => {
 		if (premiumUsers.includes(message.author.id)) {
 			if (!message.guild.member(bot.user).hasPermission("BAN_MEMBERS")) return message.reply("I do not have permissions to ban members in this server.").catch(function() {});
 			message.reply("Attempting to ban all scammers from this server (if any).")
-			var scammers = await bot.channels.get("420745256439513089").fetchMessages({ limit: 100 });
+			var scammers = await bot.channels.get("444588565154889738").fetchMessages({ limit: 100 });
 			scammers = scammers.map(user => user.content);
-			var verifiedPeople = await bot.channels.get("423189481349185547").fetchMessages({ limit: 100 });
+			var verifiedPeople = await bot.channels.get("443931376287481858").fetchMessages({ limit: 100 });
 			verifiedPeople = verifiedPeople.map(user => user.content.split(" "));
 			var membersToBan = verifiedPeople.filter(user => scammers.includes(user[1]));
 			membersToBan.forEach(member => {
