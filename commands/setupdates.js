12d4248d8d7f1efdd46e3f2bdc2e6bf897a5f0f1
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-05-11 16:21:35 -0400

    Update setupdates.js

--- commands/setupdates.js
+++ commands/setupdates.js
@@ -7,7 +7,7 @@ if (member.roles.get("400511826745360405") //comanager
   let update = message.content.substr(12);
 
 let channel = bot.channels.find(`id`, "444588564328742926")
-let editor = await channel.fetchMessage("429100390206668800")
+let editor = await channel.fetchMessage("444593973756166155")
 await editor.edit(update)
 await message.react("\u2705")
 
