0fd3473acf047402ecec2f7014fec3cf6ac732b5
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-05-11 16:21:46 -0400

    Update updates.js

--- commands/updates.js
+++ commands/updates.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
 let channel = bot.channels.find(`id`, "444588564328742926")
-let editor = await channel.fetchMessage("429100390206668800")
+let editor = await channel.fetchMessage("444593973756166155")
 let thing = new Discord.RichEmbed()
 	.setTitle("Updates")
  .setColor("#FF0000")
