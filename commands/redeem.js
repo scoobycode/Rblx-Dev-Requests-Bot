45be0352b1297b0eeb77cce01890104c574991ef
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-05-11 16:03:23 -0400

    Update redeem.js

--- commands/redeem.js
+++ commands/redeem.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 var random = require('random-string');
 module.exports.run = async (bot, message, args) => {
 let userid = message.author.id
-let userschannel = bot.channels.find(`id`, "433791777468841996")
+let userschannel = bot.channels.find(`id`, "444588564056113162")
 let pusers = await userschannel.fetchMessages({
         limit: 100
 })
@@ -10,7 +10,7 @@ let usercheck = pusers.find(m => m.content === userid)
 if(usercheck) return message.reply("You already have premium!")
 let code = args[0]
 if(!code) return message.reply("Please include the code to redeem!")
-let codeschannel = bot.channels.find(`id`, "433791740387000341")
+let codeschannel = bot.channels.find(`id`, "444588560859791381")
                 let codes = await codeschannel.fetchMessages({
                         limit: 100
                 })
