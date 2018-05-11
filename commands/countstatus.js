ab75f88329cb5cd16c7782f62a7b652238e8d74b
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-05-11 19:30:09 -0400

    Update countstatus.js

--- commands/countstatus.js
+++ commands/countstatus.js
@@ -1,10 +1,9 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
-let guild = bot.guilds.find(`id`, "400508946709872660")
-let member = await guild.fetchMember(message.author.id)
-if(!member) return;
-if (member.roles.get("400511826745360405") //comanager
- || member.roles.get("400511217061330955")) { //owner 
+let guild = bot.guilds.find(`id`, "443867131721941005");;
+	let member = await guild.fetchMember(message.author.id)
+	if (!member) return;
+	if (member && member.roles.get("443867603103121410")) { //developer
 if (bot.counter === false) {
 bot.user.setActivity(`${bot.guilds.size} servers`, {type: "WATCHING"});
       message.react("\u2705")
