ae6f4700eafccb8e6a7a5a744cb33fad947d1c09
ethanlaj <24838272+ethanlaj@users.noreply.github.com>
2018-05-27 13:42:57 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,9 @@ const fs = require("fs");
 const DBL = require('dblapi.js');
 const bot = new Discord.Client({ disableEveryone: true });
 bot.counter = false;
-//let blacklist = require("./blacklist.json")
 process.on('unhandledRejection', console.error)
 const request = require('request-promise-native');
-
 bot.commands = new Discord.Collection();
-
 fs.readdir("./commands/", (err, files) => {
 	if (err) console.log(err);
 	let jsfile = files.filter(f => f.split(".").pop() === "js")
@@ -24,23 +21,20 @@ fs.readdir("./commands/", (err, files) => {
 	});
 });
 function postServerCount() {
-    return request.post({
-        uri: `https://discordbots.org/api/bots/${bot.user.id}/stats`,
-        headers: {
-            Authorization: process.env.dbl,
-        },
-        json: true,
-        body: {
-            server_count: bot.guilds.size,
-        },
-    });
+	return request.post({
+		uri: `https://discordbots.org/api/bots/${bot.user.id}/stats`,
+		headers: {
+			Authorization: process.env.dbl,
+		},
+		json: true,
+		body: {
+			server_count: bot.guilds.size,
+		},
+	});
 }
 bot.on("ready", async () => {
+	console.log(`${bot.user.username} is online!`)
 	postServerCount()
-	console.log(`${bot.user.username} is online!`);
-	let tchannel = bot.channels.find(`id`, `444588562550358016`);
-	let ochannel = bot.channels.find(`id`, `444588562961268768`);
-	await tchannel.bulkDelete(100)
 	let upvotesholdingchannel = bot.channels.find("id", "448615839533498388");
 	let upvotessend = bot.channels.find("id", "448951130081460245");
 	var upvoter;
@@ -60,11 +54,8 @@ bot.on("ready", async () => {
 			url: user.presence.game.url
 		});
 	});
-	await ochannel.bulkDelete(100)
-	//await achannel.bulkDelete(100)
 	await bot.user.setActivity("for !help", { type: "WATCHING" });
 });
-
 bot.on("presenceUpdate", function(oldMember, newMember) {
 	if (oldMember.user.id === "291367352476631040") {
 		if (newMember.presence.game !== null) {
@@ -78,36 +69,26 @@ bot.on("presenceUpdate", function(oldMember, newMember) {
 	}
 });
 bot.on("guildCreate", async guild => {
-	let hello = new Discord.RichEmbed()
-		.setTitle("Thanks For Adding Me To Your Server!")
-		.setColor("#0000ff")
-		.setDescription("Thanks for inviting Scam reports bot to your server!\nScam reports bot is owned by Scooby and ethan and was made by the Co-Owner, @ethanlaj#8805. For a list of commands, just say \`!help\`\nIf you need any help what so ever, feel free to join our support server!\nInvite link: https://discord.gg/53Jgcu5");
-	let hichannels = guild.channels.filter(c => c.type === "text")
-	let ahichannels = hichannels.filter(c => c.permissionsFor(bot.user).has("READ_MESSAGES"));
-	let fhichannel = ahichannels.filter(c => c.permissionsFor(bot.user).has("SEND_MESSAGES"));
-	let hichannel = fhichannel.first();
-	if (hichannel) {
-		await hichannel.send(hello);
-	}
 	postServerCount()
+	let welcomeMessage = new Discord.RichEmbed().setTitle("Thanks For Adding Me To Your Server!").setColor("#0000ff").setDescription("Thanks for inviting Scam reports bot to your server!\nScam reports bot is owned by Scooby and ethan and was made by the Co-Owner, @ethanlaj#8805. For a list of commands, just say \`!help\`\nIf you need any help what so ever, feel free to join our support server!\nInvite link: https://discord.gg/53Jgcu5");
+	let hichannel = guild.channels.filter(c => c.type === "text" && c.permissionsFor(bot.user).has("READ_MESSAGES") && c.permissionsFor(bot.user).has("SEND_MESSAGES")).first()
+	if (hichannel) hichannel.send(welcomeMessage);
 	if (bot.counter) await bot.user.setActivity(`${bot.guilds.size} servers`, { type: "WATCHING" });
 });
-
 bot.on("guildDelete", async guild => {
 	postServerCount()
 	if (bot.counter) await bot.user.setActivity(`${bot.guilds.size} servers`, { type: "WATCHING" });
 });
-
 bot.on("message", async message => {
 	let upvotesholdingchannel = bot.channels.find("id", "448615839533498388");
 	let upvotessend = bot.channels.find("id", "448951130081460245");
 	var upvoter;
-	if (message.channel === upvotesholdingchannel) { 
+	if (message.channel === upvotesholdingchannel) {
 		upvoter = await bot.fetchUser(message.content)
 		upvotessend.send(`Many thanks to ${upvoter.tag} for upvoting our bot!`).then(() => {
 			message.delete();
-			})
-}
+		})
+	}
 	if (message.author.bot) return;
 	if (message.channel.type === "dm") return;
 	let messageArray = message.content.split(" ");
@@ -125,17 +106,14 @@ bot.on("message", async message => {
 	}
 	const aprefix = await getPrefix(bot, message, args)
 	if (aprefix) var prefix = aprefix;
-	//console.log(`${prefix} second`)
 	if (!aprefix) var prefix = botconfig.prefix;
-	// console.log(`${prefix} third`)
 	if ((message.isMemberMentioned(bot.user)) && (message.content.endsWith("prefix"))) {
 		return message.reply(`My prefix is \`${prefix}\``);
 	}
 	if ((message.author === bot.user) && (message.channel.id === "434152264135868418") && (message.content.endsWith("request timeout"))) {
 		message.delete(180000);
 	}
-	if ((message.isMemberMentioned(bot.user)) &&
-		(message.content.endsWith("prefix reset")) && (message.member.hasPermission("MANAGE_GUILD"))) {
+	if ((message.isMemberMentioned(bot.user)) && (message.content.endsWith("prefix reset")) && (message.member.hasPermission("MANAGE_GUILD"))) {
 		let aaa = dbguild.channels.filter(m => RegExp("prefix-database", "gi").test(m.name));
 		aaa.forEach(chl => {
 			chl.fetchMessages({ limit: 100 }).then(msgs => {
@@ -153,7 +131,4 @@ bot.on("message", async message => {
 	if (!commandfile) return;
 	return commandfile.run(bot, message, args, prefix);
 });
-
-
-
 bot.login(botconfig.token);
