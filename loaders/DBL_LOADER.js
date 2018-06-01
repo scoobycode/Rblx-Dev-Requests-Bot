fd29fa144784a75d5cc1992ed491a43a4e59b6bc
Yikey <gtc122004@gmail.com>
2018-06-01 19:33:01 -0400

    Update DBL_LOADER.js

--- loaders/DBL_LOADER.js
+++ loaders/DBL_LOADER.js
@@ -1,17 +1,17 @@
 const REQUEST = require("request-promise-native");
 
 module.exports = {
-    id: "DBL_LOADER",
-    exec: (client) => {
-        request.post({
-            uri: `https://discordbots.org/api/bots/${client.user.id}/stats`,
-            headers: {
-                Authorization: process.env.dbl,
-            },
-            json: true,
-            body: {
-                server_count: client.guilds.size,
-            },
-        });
-    }
+	id: "DBL_LOADER",
+	exec: (client) => {
+		request.post({
+			uri: `https://discordbots.org/api/bots/${client.user.id}/stats`,
+			headers: {
+				Authorization: process.env.dbl,
+			},
+			json: true,
+			body: {
+				server_count: client.guilds.size,
+			},
+		});
+	}
 }
