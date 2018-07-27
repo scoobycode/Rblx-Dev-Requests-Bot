e7d4baac085e68697614202caff0b7c8efe221d4
noanimeallowed <35615829+scoobycode@users.noreply.github.com>
2018-07-27 13:26:07 -0700

    Update server.js

--- commands/server.js
+++ commands/server.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
 
-message.reply("**Scam Reports Support Server:** https://discord.gg/53Jgcu5")
+message.reply("**Scam Reports Support Server:** https://discord.gg/3dECRh8")
 
 
 
